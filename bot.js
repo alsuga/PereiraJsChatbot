5e0e21cc3c3da648240bdd687e90c1bda5d8f2f7
Alejandro <ingalsuga@gmail.com>
2017-05-09 01:00:38 -0500

    Toy reservation bot

--- bot.js
+++ bot.js
@@ -12,10 +12,34 @@ const telegramSettings = {
 const telegramBot = new TelegramBot(telegramSettings);
 botmaster.addBot(telegramBot);
 
+const reservations = new Map();
+
+const controller = (bot, update) => {
+    const [command, ...messages] = update.message.text.split(" ");
+    if(command === '/reserve') {
+        let textMessage;
+        if(reservations.get(update.sender.id)) {
+            textMessage = 'You already made your reservation';
+        } else {
+            if(messages.length !== 2) {
+                textMessage = 'You must send your name and your email';
+            } else {
+                textMessage = 'Thanks for your reservation';
+                reservations.set(update.sender.id, messages);
+            }
+        }
+        return bot.reply(update, textMessage);
+    }
+    if(command === '/reservations') {
+        return bot.reply(update, `There are ${reservations.size} reservations`);
+    }
+    const helpMessage = 'Hi, If you want to make a reservation write "/reserve YOUR_NAME YOUR_EMAIL" '
+    + 'or "/reservations" to see how many reservations are';
+    return bot.reply(update, helpMessage)
+}
+
 botmaster.use({
   type: 'incoming',
   name: 'my-middleware',
-  controller: (bot, update) => {
-    return bot.reply(update, 'Hello world!');
-  }
+  controller: controller,
 });
